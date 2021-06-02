779646025f3a07a992a4d960c270de9a78fb48fd
FanGaoXS <954278478@qq.com>
2021-06-02 18:33:10 +0800

    chat@wqk 创建仓库

---
+++ server-chat/src/main/java/cn/fangao/serverchat/ServerChatApplication.java
@@ -0,0 +1,13 @@
+package cn.fangao.serverchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerChatApplication.class, args);
+    }
+
+}
---
+++ web-chat/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
